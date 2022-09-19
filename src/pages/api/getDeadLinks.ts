a3a6d13ceeb4852d15cf6896a3988dfb72d7c620
Cody Partington <codythatsme@gmail.com>
2022-09-20 08:51:01 +1000

    new crawl API applied

--- src/pages/api/getDeadLinks.ts
+++ src/pages/api/getDeadLinks.ts
@@ -8,46 +8,53 @@ const url = req.body.newUrl
 
     const newUrls = req.body.allowedSubdomains
 
-const getUrl = (link: string) => {
-    if (link.includes('http')) {
-        return link
-    } else if (link.startsWith('/')) {
-        // console.log(`SHOURTCUT ${link}` )
-        return `${url}/${link}`
-    } else {
-        return `${url}/${link}`
+    const getUrl = (link: string) => {
+        if (link.includes('http')) {
+            return link
+        } else if (link.startsWith('/')) {
+            // console.log(`SHOURTCUT ${link}` )
+            return `${url}/${link}`
+        } else {
+            return `${url}/${link}`
+        }
     }
-}
 
-const seenUrls = {} as any
-const foundOldLinks = {} as any
-let crawlCount = 0
-const oldLinks = [] as any
+    const seenUrls = {} as any
+    const foundOldLinks = {} as any
+    let crawlCount = 0
+    let pageFinished: any
+    const crawl = async (url: string) => {
+        pageFinished = false
+        seenUrls[url] = true
+        crawlCount++
+        const res = await fetch(url)
+        const html = await res.text()
+        const $ = cheerio.load(html)
+        const links = $("a").map((i, link) => link.attribs.href).get()
 
-const crawl = async (url: string) => {
-    if (seenUrls[url]) return
-    seenUrls[url] = true
-    crawlCount ++
-    const res = await fetch(url)
-    const html = await res.text()
-    const $ = cheerio.load(html)
-    const links = $("a").map((i, link) => link.attribs.href).get()
+        const { host } = urlParser.parse(url) as any
 
-    const { host } = urlParser.parse(url) as any
+        const hostLinks = links.filter((link, i) => {
+            return link.includes(host)
+        })
 
-    links.filter(link => link.includes(host)).forEach(link => {
-        if (!newUrls.some((website: any) => link.toLowerCase().includes(website.toLowerCase()))) {
-            // console.log(`Old Link: ${link} found on URL ${url}`);
-            foundOldLinks[url] = foundOldLinks[url] ? [...foundOldLinks[url], link] : [link]
+        for (let link of hostLinks) {
+            if (!newUrls.some((website: any) => link.toLowerCase().includes(website.toLowerCase()))) {
+                console.log(`Old Link: ${link} found on URL ${url}`);
+                foundOldLinks[url] = foundOldLinks[url] ? [...foundOldLinks[url], link] : [link]
+            }
+            if (link.includes(newUrls[0]) && !seenUrls[getUrl(link)]) {
+                await crawl(getUrl(link))
+            }
         }
-        if (link.includes(newUrls[0])) {
-            crawl(getUrl(link))
-        }
-    })
-}
+    }
 
-// if same old link found on multiple pages, flag that.
-crawl(url)
+    const crawlAll = async () => {
+        const results = await crawl(url)
+
+    }
+
+    crawlAll()
 }
 
 export default getDeadLinks
