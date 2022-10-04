e312e695324a9d9e11087d9a610502f8c4a0a80f
Cody Partington <codythatsme@gmail.com>
2022-10-04 23:59:51 +1100

    auto animate working (kinda?)

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
-const Results = ({results, setResults }: any) => {
+
+
+const Results = ({ results, setResults }: any) => {
+  
+  const [parent] = useAutoAnimate()
   
   const removeItem = (i: number, iKey: number) => {
     const newResults = results
@@ -14,16 +19,16 @@ const Results = ({results, setResults }: any) => {
 
   return (
     <>
-        <div className="flex flex-col items-center justify-center min-h-screen bg-slate-900">
           <h1 className="text-5xl text-white">Results</h1>
+        <div ref={parent as any} className="flex flex-col items-center justify-center min-h-screen bg-slate-900">
           {results.map((obj:any, iKey:any) => (
             <div key={iKey} className="flex w-full flex-col items-center justify-center border-b-4 border-gray-500 p-4">
               <div className='flex w-full items-center justify-center'>
               <a href={obj.url} className="text-2xl text-red-500 mb-6 text-center">{obj.url.substr(0, obj.url.lastIndexOf(".aspx"))}</a>
               </div>
-              <ul className='flex flex-col w-full'>
+              <ul ref={parent as any} className='flex flex-col w-full'>
                 {obj.links.map((link: string, i: number) => (
-                <div key={i} className='flex w-full justify-between p-2'>
+                <div key={i} className='flex w-full justify-between p-2 bg-slate-800 m-2'>
                 <li className="text-white p-4">{link}</li>
                 <button onClick={() => removeItem(i, iKey)} className=' text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:border-green-400 dark:text-green-400 dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-900 transition-ease-in duration-200'>Complete</button>
                 </div>
