fcf56b0a0bf583d0438d7cf7e1914966967b6ffd
Cody Partington <codythatsme@gmail.com>
2022-10-24 11:52:06 +1100

    added UI for page counter

---
+++ src/components/CountPages.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const CountPages = ({ setResults, setIsLoading }: any) => {
+    const [searchUrl, setSearchUrl] = React.useState("");
+    const [pageCount, setPageCount] = React.useState(0);
+
+  const getPageCount = async () => {
+    setIsLoading(true);
+    const res = await fetch("https://ded-linx-api-production.up.railway.app/api/v1/countPages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        searchUrl,
+      }),
+    });
+    const data = await res.json();
+    setPageCount(data);
+    setIsLoading(false);
+  };
+
+    return pageCount ? (
+    <>
+      <div className="w-[75%] items-center flex flex-col justify-center mb-[20rem]">
+                <p className="text-white text-6xl">Found {pageCount} pages</p>
+      </div>
+    </>
+    ) : (
+               <>
+      <div className="w-[75%] items-center flex flex-col">
+        <div className="flex justify-between w-[33%]">
+          <input
+            value={searchUrl}
+            onChange={(e) => setSearchUrl(e.target.value)}
+            className="mb-10 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            type="text"
+            placeholder="Website to Search"
+          />
+        </div>
+      </div>
+      <button
+        onClick={() => getPageCount()}
+        type="button"
+        className="mb-[15rem] w-[10rem] text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 transition-ease-in duration-200"
+      >
+        find em
+      </button>
+    </>
+    )
+};
+
+export default CountPages;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
+import CountPages from "../components/CountPages";
 import FindDead from "../components/FindDead";
 import FindTarget from "../components/FindTarget";
 import Results from "../components/Results";
@@ -9,7 +10,7 @@ import { trpc } from "../utils/trpc";
 const Home: NextPage = () => {
   // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
-  const [tab, setTab] = React.useState([true, false]);
+  const [tab, setTab] = React.useState([true, false, false]);
   const [results, setResults] = React.useState(null);
   const activeClasses = "text-gray-900 bg-gray-100 dark:bg-gray-700 dark:text-white"
   const inactiveClasses = "hover:text-gray-700 hover:bg-gray-50 dark:hover:text-white bg-gray-800 dark:hover:bg-gray-700"
@@ -43,15 +44,19 @@ const [isLoading, setIsLoading] = React.useState(false)
           <nav className="mt-[2rem] mb-[3rem] w-[50%] items-center flex flex-col">
 <ul className="text-sm font-medium text-center text-gray-500 rounded-lg divide-x divide-gray-200 shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
     <li className="w-full">
-        <a onClick={() =>setTab([true, false])} className={`inline-block p-4 w-full focus:outline-none rounded-l-lg min-w-[10rem] cursor-pointer ${tab[0] && activeClasses} ${!tab[0] && inactiveClasses}`} aria-current="page">Old Links</a>
+        <a onClick={() =>setTab([true, false, false])} className={`inline-block p-4 w-full focus:outline-none rounded-l-lg min-w-[10rem] cursor-pointer ${tab[0] && activeClasses} ${!tab[0] && inactiveClasses}`} aria-current="page">Old Links</a>
     </li>
     <li className="w-full">
-              <a onClick={() =>setTab([false, true])} className={`inline-block p-4 w-full focus:outline-none rounded-r-lg min-w-[10rem] cursor-pointer ${tab[1] && activeClasses} ${!tab[1] && inactiveClasses}`}>Specific Links</a>
+              <a onClick={() =>setTab([false, true, false])} className={`inline-block p-4 w-full focus:outline-none min-w-[10rem] cursor-pointer ${tab[1] && activeClasses} ${!tab[1] && inactiveClasses}`}>Specific Links</a>
+    </li>
+    <li className="w-full">
+              <a onClick={() =>setTab([false, false, true])} className={`inline-block p-4 w-full focus:outline-none rounded-r-lg min-w-[10rem] cursor-pointer ${tab[2] && activeClasses} ${!tab[1] && inactiveClasses}`}>Specific Links</a>
     </li>
 </ul>
         </nav>
         {tab[0] && <FindDead setResults={setResults} setIsLoading={setIsLoading} />}
         {tab[1] && <FindTarget setResults={setResults} setIsLoading={setIsLoading} />}
+        {tab[2] && <CountPages setResults={setResults} setIsLoading={setIsLoading} />}
 
       </div>
       {results && (<div className="min-h-screen bg-slate-900 flex flex-col items-center justify-center">
